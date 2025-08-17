67038ea1a5f28d3c304b0df36cbd1ca95b66b0c9
Reddy Mani Simha <itsmanisimha@gmail.com>
2025-08-17 18:57:11 +0530

    datatypesnotes

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+"usestrict";
+// alert("This file is deprecated and will be removed in a future release. Please use the new datatypes module instead.");
+// we are using the node js this only works in html embedded html files
+console.log(3+4);
+// code redability should be very high
+// aler is only browser only function and node js works outside the browser
+// Primitives in JavaScript include:
+// number is only upto 2^53 - 1
+// BigInt is used for larger numbers
+// boolean is true or false
+// null is a standalone value that represents no value
+// undefined is a variable that has been declared but not assigned a value
+// symbol is a unique and immutable value(unique)
+// objects are collections of key-value pairs
+console.log(typeof null); 
+// object
+console.log(typeof undefined); 
+// undefined
