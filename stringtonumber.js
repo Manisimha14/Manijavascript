a8493c04489c2d076f12ca154bbb34faf54f574f
Reddy Mani Simha <itsmanisimha@gmail.com>
2025-08-19 11:46:28 +0530

    string to number conversion

---
+++ stringtonumber.js
@@ -0,0 +1,10 @@
+console.log(2**3); // 8 
+// Exponentiation operator
+// post and pre increment/decrement operators
+let a=5;
+let b=++a; // pre-increment
+console.log(b); // 6
+console.log(a); // 6
+let c=a++; // post-increment    
+console.log(c); // 6
+console.log(a); // 7
