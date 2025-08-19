20c2b728256afc273726eac5232a991465877aed
Reddy Mani Simha <itsmanisimha@gmail.com>
2025-08-19 12:00:06 +0530

    more on comparision of datatypes in java script

---
+++ comparisionofdatatypes.js
@@ -0,0 +1,12 @@
+console.log(2 > 1); // true
+console.log(2 < 1); // false
+// Comparison operators
+// Java script code to convert various data types
+console.log(2 != 1); // true
+// Not equal operator (!=)
+console.log(null == undefined); // true
+// Null and undefined are loosely equal
+console.log (null > 0); // false
+console.log (null < 0); // false 
+console.log (null >= 0); // true   
+// === checks for both value and type
