1847280807020800be2ff982ee827a35965bfa73
Reddy Mani Simha <itsmanisimha@gmail.com>
2025-08-20 14:33:55 +0530

    learn basic stack and memory concept in js

---
+++ stackheap.js
@@ -0,0 +1,7 @@
+// how memory works in java script
+// stack and heap memory
+// primitive types are stored in stack memory
+// non primitive types are stored in heap memory
+// stack is used for reference variable and heap is used to store the value or object of the reference variable
+// in memory the reference variable is matche with the value or object in heap memory
+// multiple reference variables can point to the same object in heap memory
