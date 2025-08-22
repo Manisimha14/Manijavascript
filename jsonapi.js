718ed25e25a947babb1b602fe48d12c7020156fe
Reddy Mani Simha <itsmanisimha@gmail.com>
2025-08-22 21:59:56 +0530

    intro to Api

---
+++ jsonapi.js
@@ -0,0 +1,15 @@
+// ******About Json and Destructuring*****
+// Dstructuring Objects
+let obj = {
+    name :"manisimha"
+}
+let {name :n} =obj
+console.log(n);
+// this is another way to get the object
+// api
+// json example
+{
+    // object without variable
+    // the data can be retreived from api can be object and array
+    
+}
